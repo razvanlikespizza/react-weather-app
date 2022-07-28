188e19ecd6b7bd7e0af7715c6ba5f3d579245e65
smrazvan <razvanro666@gmail.com>
2022-07-28 20:36:35 +0300

    modifined ignore file

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const app = express();
 
 if(process.env.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname + "/build")));
-    app.get("/", (req, res) => {
-        console.log("GETTT");
-        res.sendFile(path.join(__dirname + "/build/index.html"));
-    });
+    // app.get("/", (req, res) => {
+    //     console.log("GETTT");
+    //     res.sendFile(path.join(__dirname + "/build/index.html"));
+    // });
 }
 
 app.get("/api/ip", (req, res) => {
